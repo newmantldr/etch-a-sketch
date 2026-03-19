4336925eef7fa32fd3166d66da58e1042ac9d8f0
Tyler <tylermatthew.newman@gmail.com>
2026-03-19 14:59:41 -0700

    Add default option of color for drawing with

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const container = document.querySelector('.container');
 
-function createGrid(width, height) {
-    const container = document.querySelector('.container');
+function createGrid(gridSize) {
+    container.innerHTML = '';
 
-    for (i = 0; i < width; i++) {
-        for (j = 0; j < height; j++){
-            const row = document.createElement('div');
-            container.appendChild(row);
+    for (i = 0; i < gridSize*gridSize; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        container.appendChild(cell);
+
+        let cellSize = 960/gridSize;
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
+
+        cell.addEventListener('mouseenter', changeColor)
+
+        function changeColor(color = 'black'){
+            cell.style.backgroundColor = color;
         }
     }
 };
 
-createGrid(16, 16);
+createGrid(16);
+
